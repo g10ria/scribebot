c5ed5f5b43666006a75972a76b32dc1a6db217f1
Gloria Zhu <gloriazhu00@gmail.com>
2024-02-09 04:37:38 -0500

    add pitch and yaw!

--- scribebot.ino
+++ scribebot.ino
@@ -17,6 +17,16 @@
 #define STEPPER_Z_STEP 32
 #define STEPPER_Z_DIR 33
 
+#define STEPPER_PITCH_1 34
+#define STEPPER_PITCH_2 35
+#define STEPPER_PITCH_3 36
+#define STEPPER_PITCH_4 37
+
+#define STEPPER_YAW_1 38
+#define STEPPER_YAW_2 39
+#define STEPPER_YAW_3 40
+#define STEPPER_YAW_4 41  // swap these
+
 #define START_BUTTON_PIN 49
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
@@ -26,22 +36,7 @@ AccelStepper stepperLeft(AccelStepper::DRIVER, STEPPER_LEFT_STEP, STEPPER_LEFT_D
 AccelStepper stepperRight(AccelStepper::DRIVER, STEPPER_RIGHT_STEP, STEPPER_RIGHT_DIR);  // clockwise is positive
 AccelStepper stepperZ(AccelStepper::DRIVER, STEPPER_Z_STEP, STEPPER_Z_DIR);              // up is positive
 
-// AccelStepper stepperPitch(AccelStepper::FULL4WIRE, 34, 35, 36, 37);
-// AccelStepper stepperPitch(8, 8,10,9,11);
-
-/*
-COREXY TRUTH TABLE:
-Y MOVEMENT:
-back: left is negative, right is positive
-forward: left is positive, right is negative
-
-X MOVEMENT:
-left: both positive
-right: both negative
-
-OTHER AXES:
-z: up is positive
-*/
+AccelStepper stepperYaw(8, STEPPER_YAW_1, STEPPER_YAW_3, STEPPER_YAW_2, STEPPER_YAW_4);
 
 // constants
 #define MAX_Y_TRAVEL 342.9    // in mm; 13.5"
@@ -52,46 +47,126 @@ z: up is positive
 #define Z_STEPS_PER_MM 200
 
 // state variables
-float x_pose = 0;
-float y_pose = 0;
-float z_pose = 0;
+float pitch_pose = 0;
+float yaw_pose = 0;
 
-float x_next = 0;
-float y_next = 0;
-float z_next = 0;
+float pitch_next = 0;
+float yaw_next = 0;
 
-// const int stepsPerRevolution = 2038;
-// Stepper myStepper = Stepper(stepsPerRevolution, 8, 10, 9, 11);
+#define NUM_LOCS 3
 
-float locs[7][3] = {
-  { 30, 30, 20 },
-  { 100, 100, 0 },
-  { 100, 150, 5 },
-  { 150, 150, 10 },
-  { 150, 100, 0 },
-  { 100, 100, 3 },
-  { 65, 65, 20 }
+// DEFLOCS
+float locs[NUM_LOCS][5] = {
+  { 10, 30, 15, 0, 0},
+  { 10, 30, 15, 1, .25 },
+  { 10, 30, 15, 0 , 0}
+  // { 150, 100, 0, 0 },
+  // { 100, 100, 3, 0 },
+  // { 65, 65, 20, 0 }
 };
 
 int currLoc = 0;
-int numLocs = 7;
+int numLocs = NUM_LOCS;
+
+int pitch_step = 0;
+bool pitch_direction = true;
+int pitch_steps_remaining = 0;
+
+int yaw_step = 0;
+bool yaw_direction = false;
+int yaw_steps_remaining = 0;
+
+unsigned long timePrevious = 0;
+unsigned long timeNow = 0;
+
+float leftDesiredSpeed = 1600.0;
+float rightDesiredSpeed = 1600.0;
+float zDesiredSpeed = 800.0;
+float pitchDesiredSpeed = 1000.0; // aka every 1000 micros
+float yawDesiredSpeed = 1000.0; // aka every 1000 micros
 
 // x, y is all we need right now
-void moveCartesian(float x_target, float y_target, float z_target) {  // in mm
+void moveCartesian(int currLoc) {
+  float x_target = locs[currLoc][0];
+  float y_target = locs[currLoc][1];
+  float z_target = locs[currLoc][2];
+  float pitch_target = locs[currLoc][3];
+  float yaw_target = locs[currLoc][4];
+
   Serial.print("moving cartesian to: ");
   Serial.print(x_target);
   Serial.print(" ");
   Serial.print(y_target);
   Serial.print(" ");
-  Serial.println(z_target);
+  Serial.print(z_target);
+  Serial.print(" ");
+  Serial.print(pitch_target);
+  Serial.print(" ");
+  Serial.println(yaw_target);
 
-  stepperLeft.moveTo(y_target * COREXY_STEPS_PER_MM - x_target * COREXY_STEPS_PER_MM);
-  stepperRight.moveTo(-y_target * COREXY_STEPS_PER_MM - x_target * COREXY_STEPS_PER_MM);
-  stepperZ.moveTo(z_target * Z_STEPS_PER_MM);
+  // syncing up the speeds; this one is a doozy lmao TODO!
+  float stepperLeftTarget = y_target * COREXY_STEPS_PER_MM - x_target * COREXY_STEPS_PER_MM;
+  float stepperRightTarget = -y_target * COREXY_STEPS_PER_MM - x_target * COREXY_STEPS_PER_MM;
+  float stepperZTarget = z_target * Z_STEPS_PER_MM;
 
-  x_next = x_target;
-  y_next = y_target;
-  z_next = z_target;
+  float stepperLeftOffset = stepperLeftTarget - stepperLeft.currentPosition();
+  float stepperRightOffset = stepperRightTarget - stepperLeft.currentPosition();
+  float stepperZOffset = stepperZTarget - stepperZ.currentPosition();
+  Serial.println(stepperLeftOffset);
+  Serial.println(stepperRightOffset);
+  Serial.println(stepperZOffset);
+
+  float maxOffset = max(abs(stepperLeftOffset), abs(stepperRightOffset));
+
+  float scaleFactor = 1600.0/maxOffset; // making them scaled relatively, one of them being 1600
+  stepperLeft.setSpeed(scaleFactor * stepperLeftOffset);
+  stepperRight.setSpeed(scaleFactor * stepperRightOffset);
+  stepperLeft.setMaxSpeed(scaleFactor * stepperLeftOffset);
+  stepperRight.setMaxSpeed(scaleFactor * stepperRightOffset);
+  Serial.println("XY STEPPER SPEEDS");
+  Serial.println(scaleFactor * stepperLeftOffset);
+  Serial.println(scaleFactor * stepperRightOffset);
+  Serial.println();
+
+  stepperLeft.moveTo(stepperLeftTarget);
+  stepperRight.moveTo(stepperRightTarget);
+  stepperZ.moveTo(stepperZTarget);
+
+  // pitch pose is in terms of steps
+  float stepperPitchTarget = pitch_target * 1300.0; // todo comment this value and verify with zeroing jig
+  float stepperPitchOffset = stepperPitchTarget - pitch_pose;
+  pitch_next = stepperPitchTarget;
+  Serial.println("pitch target info"); Serial.println(stepperPitchTarget); Serial.println(stepperPitchOffset);
+  pitch_steps_remaining = abs(stepperPitchOffset);
+  pitch_direction = stepperPitchOffset > 0;
+  pitch_step = 0;
+
+  // yaw pose is in terms of steps
+  float stepperYawTarget = yaw_target * 8043.0;
+  float stepperYawOffset = stepperYawTarget - yaw_pose;
+  yaw_next = stepperYawTarget;
+  Serial.println("yaw target info"); Serial.println(stepperYawTarget); Serial.println(stepperYawOffset);
+  yaw_steps_remaining = abs(stepperYawOffset);
+  yaw_direction = stepperYawOffset > 0;
+  yaw_step = 0;
+
+  float leftFastestTime = abs(stepperLeftOffset) / leftDesiredSpeed;
+  float rightFastestTime = abs(stepperRightOffset) / rightDesiredSpeed;
+  float zFastestTime = abs(stepperZOffset) / zDesiredSpeed;
+  float pitchFastestTime = abs(stepperPitchOffset) / pitchDesiredSpeed;
+  float yawFastestTime = abs(stepperYawOffset) / yawDesiredSpeed;
+
+  float maximalTimeNeeded = max(leftFastestTime, max(rightFastestTime, max(zFastestTime, max(pitchFastestTime, yawFastestTime))));
+  Serial.println("Max time needed: ");
+  Serial.println(maximalTimeNeeded);
+  Serial.println("Proposed speeds:");
+  Serial.println(stepperLeftOffset/maximalTimeNeeded);
+  Serial.println(stepperRightOffset/maximalTimeNeeded);
+  Serial.println(stepperZOffset/maximalTimeNeeded);
+  Serial.println(stepperPitchOffset/maximalTimeNeeded);
+  Serial.println(stepperYawOffset/maximalTimeNeeded);
+
+  // TODO implement min/max speed logic
 }
 
 void setup() {
@@ -111,15 +186,16 @@ void setup() {
   stepperZ.setAcceleration(10000);
   stepperZ.setSpeed(800);
 
-  // stepperRight.moveTo(400);
-  // stepperLeft.moveTo(400);
-  // stepperZ.moveTo(100);
+  // set up pitch/yaw output pins
+  pinMode(STEPPER_PITCH_1, OUTPUT);
+  pinMode(STEPPER_PITCH_2, OUTPUT);
+  pinMode(STEPPER_PITCH_3, OUTPUT);
+  pinMode(STEPPER_PITCH_4, OUTPUT);
 
-  // stepperPitch.setMaxSpeed(1000000);
-  // stepperPitch.setAcceleration(1000000);
-  // stepperPitch.setSpeed(1000000);
-
-  // stepperPitch.moveTo(2048);
+  pinMode(STEPPER_YAW_1, OUTPUT);
+  pinMode(STEPPER_YAW_2, OUTPUT);
+  pinMode(STEPPER_YAW_3, OUTPUT);
+  pinMode(STEPPER_YAW_4, OUTPUT);
 
   // set up limit switch pins
   pinMode(Y_LIMIT_SWITCH_PIN, INPUT);
@@ -128,12 +204,13 @@ void setup() {
 
   // home the system
   // home();
+  // moveCartesian(0);
 
   lcd.init();
   lcd.backlight();
   display("hello scribebot!", 0, true);
 
-  Serial.println("hello!");
+  Serial.println("Finished setup!");
 }
 
 #define GCODE_LINE_LENGTH 41
@@ -150,7 +227,57 @@ bool button_pressed_temp = true;
 
 bool simulate_buffer = true;
 
+void updatePitchYawState() {
+  pitch_pose = pitch_next;
+  yaw_pose = yaw_next;
+}
+
+void updatePitchSteps() {
+  // pitch
+  if (pitch_direction == 1) {
+    pitch_step++;
+  } else if (pitch_direction == 0) {
+    pitch_step--;
+  }
+
+  if (pitch_step > 7) { pitch_step = 0; }
+  if (pitch_step < 0) { pitch_step = 7; }
+}
+
+void updateYawSteps() {
+  if (yaw_direction == 1) {
+    yaw_step++;
+  } else if (yaw_direction == 0) {
+    yaw_step--;
+  }
+
+  if (yaw_step > 7) { yaw_step = 0; }
+  if (yaw_step < 0) { yaw_step = 7; }
+}
+
 void loop() {
+  // return;
+  // runMotors();
+  // return;
+  // while (stepsRemaining > 0)  // steps remaining until the full circle
+  // {
+  //   timeNow = micros();  //write down the current time in microseconds
+  //   if (timeNow - timePrevious >= 2000) {
+  //     stepPitch();  //call the stepper function (check void stepper below)
+  //     stepYaw();
+  //     timePrevious = micros();
+  //     stepsRemaining--;
+  //     updateSteps();
+  //   }
+  // }
+
+  // Serial.println("Wait..!");
+  // delay(1000);
+  // direction = !direction;  // opposite direction
+  // stepsRemaining = 300;    // reset the number of steps
+
+  // return;
+
   if (!sd_inserted) {  // sd not inserted yet, check if inserted
     Serial.println("waiting for sd");
     display("Waiting for SD...", 0, false);
@@ -178,14 +305,15 @@ void loop() {
           startSDBuffer();
         }
       }
-    } else {  // we are mid job
-      if (reachedTarget()) {
+    } else {                  // we are mid job
+      if (reachedTarget()) {  // update next target location
+
+        updatePitchYawState();  // only necessary for pitch and yaw :P
+
         if (simulate_buffer) {
-          // stepperLeft.setSpeed(800);
-          // stepperRight.setSpeed(800);
-          // stepperZ.setSpeed(800);
+          // READ FROM SIMULATION ---------------------
           if (currLoc < numLocs) {
-            moveCartesian(locs[currLoc][0], locs[currLoc][1], locs[currLoc][2]);
+            moveCartesian(currLoc);
             currLoc++;
           } else {
             display("JOB COMPLETED!", 0, true);
@@ -194,7 +322,9 @@ void loop() {
 
             currLoc = 0;
           }
+          // READ FROM SIMULATION ---------------------
         } else {
+          // READ FROM SD ---------------------
           if (numLinesRead < numLines) {  // job not done, keep reading into buffer
             updateSDBuffer();
           } else {  // we are done!
@@ -202,11 +332,155 @@ void loop() {
             delay(1000);
             mid_job = false;
           }
+          // READ FROM SD ---------------------
         }
       }
-      stepperLeft.run();
-      stepperRight.run();
-      stepperZ.run();
+
+      runMotors();
+    }
+  }
+}
+
+void stepPitch() {
+  switch (pitch_step) {
+    case 0:
+      digitalWrite(STEPPER_PITCH_1, LOW);
+      digitalWrite(STEPPER_PITCH_2, LOW);
+      digitalWrite(STEPPER_PITCH_3, LOW);
+      digitalWrite(STEPPER_PITCH_4, HIGH);
+      break;
+    case 1:
+      digitalWrite(STEPPER_PITCH_1, LOW);
+      digitalWrite(STEPPER_PITCH_2, LOW);
+      digitalWrite(STEPPER_PITCH_3, HIGH);
+      digitalWrite(STEPPER_PITCH_4, HIGH);
+      break;
+    case 2:
+      digitalWrite(STEPPER_PITCH_1, LOW);
+      digitalWrite(STEPPER_PITCH_2, LOW);
+      digitalWrite(STEPPER_PITCH_3, HIGH);
+      digitalWrite(STEPPER_PITCH_4, LOW);
+      break;
+    case 3:
+      digitalWrite(STEPPER_PITCH_1, LOW);
+      digitalWrite(STEPPER_PITCH_2, HIGH);
+      digitalWrite(STEPPER_PITCH_3, HIGH);
+      digitalWrite(STEPPER_PITCH_4, LOW);
+      break;
+    case 4:
+      digitalWrite(STEPPER_PITCH_1, LOW);
+      digitalWrite(STEPPER_PITCH_2, HIGH);
+      digitalWrite(STEPPER_PITCH_3, LOW);
+      digitalWrite(STEPPER_PITCH_4, LOW);
+      break;
+    case 5:
+      digitalWrite(STEPPER_PITCH_1, HIGH);
+      digitalWrite(STEPPER_PITCH_2, HIGH);
+      digitalWrite(STEPPER_PITCH_3, LOW);
+      digitalWrite(STEPPER_PITCH_4, LOW);
+      break;
+    case 6:
+      digitalWrite(STEPPER_PITCH_1, HIGH);
+      digitalWrite(STEPPER_PITCH_2, LOW);
+      digitalWrite(STEPPER_PITCH_3, LOW);
+      digitalWrite(STEPPER_PITCH_4, LOW);
+      break;
+    case 7:
+      digitalWrite(STEPPER_PITCH_1, HIGH);
+      digitalWrite(STEPPER_PITCH_2, LOW);
+      digitalWrite(STEPPER_PITCH_3, LOW);
+      digitalWrite(STEPPER_PITCH_4, HIGH);
+      break;
+    default:
+      digitalWrite(STEPPER_PITCH_1, LOW);
+      digitalWrite(STEPPER_PITCH_2, LOW);
+      digitalWrite(STEPPER_PITCH_3, LOW);
+      digitalWrite(STEPPER_PITCH_4, LOW);
+      break;
+  }
+}
+
+void stepYaw() {
+  switch (yaw_step) {
+    case 0:
+      digitalWrite(STEPPER_YAW_1, LOW);
+      digitalWrite(STEPPER_YAW_2, LOW);
+      digitalWrite(STEPPER_YAW_3, LOW);
+      digitalWrite(STEPPER_YAW_4, HIGH);
+      break;
+    case 1:
+      digitalWrite(STEPPER_YAW_1, LOW);
+      digitalWrite(STEPPER_YAW_2, LOW);
+      digitalWrite(STEPPER_YAW_3, HIGH);
+      digitalWrite(STEPPER_YAW_4, HIGH);
+      break;
+    case 2:
+      digitalWrite(STEPPER_YAW_1, LOW);
+      digitalWrite(STEPPER_YAW_2, LOW);
+      digitalWrite(STEPPER_YAW_3, HIGH);
+      digitalWrite(STEPPER_YAW_4, LOW);
+      break;
+    case 3:
+      digitalWrite(STEPPER_YAW_1, LOW);
+      digitalWrite(STEPPER_YAW_2, HIGH);
+      digitalWrite(STEPPER_YAW_3, HIGH);
+      digitalWrite(STEPPER_YAW_4, LOW);
+      break;
+    case 4:
+      digitalWrite(STEPPER_YAW_1, LOW);
+      digitalWrite(STEPPER_YAW_2, HIGH);
+      digitalWrite(STEPPER_YAW_3, LOW);
+      digitalWrite(STEPPER_YAW_4, LOW);
+      break;
+    case 5:
+      digitalWrite(STEPPER_YAW_1, HIGH);
+      digitalWrite(STEPPER_YAW_2, HIGH);
+      digitalWrite(STEPPER_YAW_3, LOW);
+      digitalWrite(STEPPER_YAW_4, LOW);
+      break;
+    case 6:
+      digitalWrite(STEPPER_YAW_1, HIGH);
+      digitalWrite(STEPPER_YAW_2, LOW);
+      digitalWrite(STEPPER_YAW_3, LOW);
+      digitalWrite(STEPPER_YAW_4, LOW);
+      break;
+    case 7:
+      digitalWrite(STEPPER_YAW_1, HIGH);
+      digitalWrite(STEPPER_YAW_2, LOW);
+      digitalWrite(STEPPER_YAW_3, LOW);
+      digitalWrite(STEPPER_YAW_4, HIGH);
+      break;
+    default:
+      digitalWrite(STEPPER_YAW_1, LOW);
+      digitalWrite(STEPPER_YAW_2, LOW);
+      digitalWrite(STEPPER_YAW_3, LOW);
+      digitalWrite(STEPPER_YAW_4, LOW);
+      break;
+  }
+}
+
+void runMotors() {
+  stepperLeft.run();
+  stepperRight.run();
+  stepperZ.run();
+
+  timeNow = micros();
+  if (timeNow - timePrevious >= 1000) {  // attempt to step pitch (and yaw later)
+
+    if (pitch_steps_remaining > 0) {
+      // Serial.println("stepping pitch");
+      stepPitch();
+      timePrevious = micros();
+      pitch_steps_remaining--;
+      updatePitchSteps();
+    }
+
+    if (yaw_steps_remaining > 0) {
+      // Serial.println("stepping yaw");
+      stepYaw();
+      timePrevious = micros();
+      yaw_steps_remaining--;
+      updateYawSteps();
     }
   }
 }
@@ -246,7 +520,7 @@ void updateTargetFromSDBuffer() {
 }
 
 bool reachedTarget() {
-  return stepperLeft.distanceToGo() == 0 && stepperRight.distanceToGo() == 0 && stepperZ.distanceToGo() == 0;
+  return stepperLeft.distanceToGo() == 0 && stepperRight.distanceToGo() == 0 && stepperZ.distanceToGo() == 0 && pitch_steps_remaining == 0 && yaw_steps_remaining == 0;
 }
 
 void estop() {  // todo make this an interrupt
@@ -255,10 +529,9 @@ void estop() {  // todo make this an interrupt
 }
 
 void home() {
-  // display("Homing XY", 1, false);
+  display("Homing XY", 1, false);
   homeXY();
-  Serial.println("asklfjadslk");
-  // display("Homing Z", 1, false);
+  display("Homing Z ", 1, false);
   homeZ();
 }
 
